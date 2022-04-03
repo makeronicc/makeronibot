278871de361c44bb88167693414fd1e08c2dc021
Eric Muir <eric.muir@xdesign.com>
2022-04-03 18:07:39 +0100

    Initial commit of Makeroni Bot V2 - created framework

--- index.js
+++ index.js
@@ -1,174 +1,139 @@
 const Discord = require("discord.js");
 const http = require("http");
 const schedule = require('node-schedule');
+const Airtable = require('airtable')
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const client = new Discord.Client();
-let discordReady = false;
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.login(BOT_TOKEN);
+const db = new Airtable({ apiKey: process.env.AIRTABLE_KEY }).base('appFwOylDMCbtHYSw');
 
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
- 
-const adapter = new FileSync('db.json')
-const db = low(adapter)
-
-db.defaults({ projects: {}, users: [], channels: [] }).write()
-
-const channels = {
-    sandbox: '756834681298747424',
-    general: '730415016716533934'
-}
-
-client.on("message", function (message) {
-  const timeTaken = Date.now() - message.createdTimestamp;
-  let content = message.content;
-  console.log(content);
-  console.log(message);
-
-  if(message.guild.name === "Makeroni") {
-      db.set('guild', message.guild.id).write();
-  }
-
-  if (message.author.bot) return;
-
-  if (content === "!ping") message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-  if (content === "!channel get id") message.reply(message.channel.id);
-  if (content === "!minutes") getMinutes().then(links => message.reply(`Here's a list of all the links posted today: ${links.join(', ')}`));
-  if (content === "!project") {
-      message.reply(`Use !project [description] to set the project you are working on, !project-finished to mark it as finished and !projects to view all the other projects people are working on.`)
-  }
-  if (content === "!projects") {
-    getProjects().then(projects => {
-        message.channel.send(projects.map(proj => `${proj.nickname} ${proj.finished ? 'was' : 'is'} working on: ${proj.project}`).join('\n'))
-    }).catch(console.log)
-  }
-  if (content === "!project-finished") {
-      db.get('projects').find({user: message.author.id, current: true}).assign({finished: true, current: false, finishedAt: message.createdTimestamp}).write();
-      message.channel.send(`Great job ${message.author} onto the next project!`)
-  }
-  if (content.startsWith('!project ')) {
-    const project = content.replace('!project ', '')
-    db.get('projects').find({user: message.author.id, current: true}).assign({finished: true, current: false, finishedAt: message.createdTimestamp}).write();
-    db.get('projects').push({
-        user: message.author.id,
-        project: project,
-        finished: false,
-        current: true,
-        createdAt: message.createdTimestamp
-    }).write();
-    message.reply(`you are now working on: ${project}`)
-  }
-  if (content.startsWith('!zoom set')) {
-      if(message.author.username === "codepope" || message.author.username === "101stArrow") {
-        const zoomLink = content.replace('!zoom set ', '')
-        db.set('zoomLink', zoomLink).write()
-        message.reply(`The new Zoom link is: ${zoomLink}`)
-      } else {
-        message.reply("Sorry, you can't set the Zoom link. If you think this is wrong ask Dj or Eric")
-      }
-  }
-});
-
-const getMessages = limit => {
-    return client.channels.fetch(channels.general).then(channel => {
-        return channel.messages.fetch({ limit: limit || 10 })
-        .then(messages => messages.map(msg => {
-            return {
-                id: msg.id,
-                content: msg.content,
-                author: {
-                    username: msg.author.username,
-                    id: msg.author.id,
-                    bot: msg.author.bot
-                },
-                channel: msg.channel.id,
-                deleted: msg.deleted,
-                timestamp: msg.createdTimestamp
-            }
-        }))
-        .then(messages => messages.sort(function(x, y){
-            return x.timestamp - y.timestamp;
-        }))
-        .catch(console.error);
-    }).catch(err => console.log(err))
-}
-
-const getMinutes = () => {
-    let start = new Date();
-    start.setHours(12);
-    start.setMinutes(0);
-    start.setSeconds(0);
-
-    let end = new Date();
-    end.setHours(18);
-    end.setMinutes(0);
-    end.setSeconds(0);
-
-    return getMessages(30).then(messages => messages.filter(msg => msg.timestamp > start && msg.timestamp < end && msg.author.bot === false && msg.content.startsWith('http'))).then(messages => {
-        return messages.map(msg => msg.content);
+const init = async () => {
+    const guilds = await client.guilds.fetch()
+    guilds.map(async (resolvable, key, collection) => {
+        await updateGuild(key)
     })
 }
 
-const getMembers = () => {
-    return client.guilds.fetch(db.get('guild').value()).then(guild => {
-        return guild.members.fetch().then(members => {
-          return members.map(member => {
-                return {
-                    joined: member.joinedTimestamp,
-                    nickname: member.nickname || member.user.username,
-                    user: member.user,
-                    deleted: member.deleted,
-                    lastMsg: member.lastMessageID,
-                    lastMsgChannel: member.lastMessageChannelID,
-                    roles: member._roles
+const shallowEqual = async (object1, object2) => {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+    return await Promise.all(keys1.map((key) => {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+      return true
+    }))
+}
+
+const fetchItemById = (dbname, key, query) => {
+    return new Promise((resolve, reject) => {
+        db(dbname).select({ filterByFormula: `${key} = ${query}` }).firstPage((err, records) => {
+            if (err) {
+                reject(err)
+            } else {
+                if (records.length == 1) {
+                    const id = records[0].id,
+                        object = records[0].fields;
+
+                    return resolve([id, object])
+                } 
+                if (records.length == 0) {
+                    return reject(`${key} not found in ${dbname}`)
                 }
-            })
-      })
-  })
-}
-
-const getProjects = () => {
-    return getMembers().then(members => {
-        return db.get('projects').value().map(proj => {
-            proj.nickname = members.filter(m => m.user.id === proj.user)[0].nickname;
-            return proj
-        });
+            }
+        })
     })
 }
 
-client.on("ready", () => {
-  console.log("We're up!")
-});
-
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
-  if (!channel) return;
-  channel.send(`Welcome to Makeroni, ${member}`);
-});
-
-
-const openDoors = schedule.scheduleJob('0 12 * * 6', date => {
-    const zoomLink = db.get('zoomLink').value()
-    client.channels.fetch(channels.general).then(channel => {
-      channel.send(`Open Doors! Come and join us on Zoom at: ${zoomLink}`);
-    }).catch(err => console.log(err))
-})
-
-const reminderMessage = schedule.scheduleJob('0 13-16 * * 6', date => {
-    const zoomLink = db.get('zoomLink').value()
-    getMessages(1).then(messages => {
-      if(messages[0].author.username !== "Makeronibot") {
-        client.channels.fetch(channels.general).then(channel => {
-            channel.send(`Come along, join us on Zoom at: ${zoomLink}`);
-        }).catch(err => console.log(err))
-      }
+const putItemById = (dbname, key, object) => {
+    return new Promise((resolve, reject) => {
+        db(dbname).update([{ id: key, fields: object }], (err, records) => {
+            if (err) reject(err)
+            else resolve(records)
+        })
     })
-})
+}
 
-const server = http.createServer((function(request,response){
-    response.writeHead(200, {"Content-Type" : "text/plain"}); 
-    response.end("OK");
-}));
-server.listen(8080);
+const createItem = async (dbname, object) => {
+    try {
+        return await db(dbname).create([{ fields: object }])
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const updateGuild = async (key) => {
+    const [id, object] = await fetchItemById('Guilds', 'Id', key)
+    const original = Object.assign({}, object);
+
+    const guild = client.guilds.cache.get(key)
+
+    object.Members = guild.memberCount
+    
+    if(!(await shallowEqual(object, original))) {
+        console.log(`Guild needs updating`)
+        await putItemById('Guilds', id, object)
+    }
+
+    await updateEvents(guild)
+}
+
+const updateEvents = async guild => {
+    const events = await guild.scheduledEvents.fetch()
+    await events.map(async (event, key) => {
+        console.log('Looking for:', key)
+        try {
+            const [id, object] = await fetchItemById('Events', 'Id', key)
+            const original = Object.assign({}, object);
+            console.log(`Found: ${id}`)
+
+            if (object["Interested Members"] !== event.userCount) {
+                object["Interested Members"] = event.userCount
+            }
+
+            if(!(await shallowEqual(object, original))) {
+                console.log(`Event needs updating`)
+                await putItemById('Events', id, object)
+            }
+
+            console.log('Completed:', key)
+
+        } catch (error) {
+            console.log(error)
+            console.log('Cannot find, creating')
+            const transformed = {
+                Id: event.id,
+                Name: event.name,
+                Description: event.description,
+                Start: event.scheduledStartTimestamp,
+                End: event.scheduledEndTimestamp,
+                "Interested Members": event.userCount
+            }
+            console.log(error)
+            await createItem('Events', transformed)
+        }
+    })
+}
+
+const updateMembers = async guild => {
+    
+}
+
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    await init();
+});
+
+client.on('interactionCreate', interaction => {
+    console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+});
+
+client.on('guildScheduledEventCreate', event => {
+    console.log(event);
+});
+
+client.login(process.env.BOT_TOKEN);
